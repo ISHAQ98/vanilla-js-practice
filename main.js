1ababb340b8c638e9b73b4ce8944f29c79d518df
ISHAQ <68194895+ISHAQ98@users.noreply.github.com>
2022-06-17 14:31:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,39 @@ const toggle = document.getElementById("toggle");
 toggle.addEventListener("change", (e) => {
   document.body.classList.toggle("dark", e.target.checked);
 });
+// Calculator try build
+/*
+const calc = () => {
+  let a = parseInt(document.querySelector("#value1").value);
+  let b = parseInt(document.querySelector("#value2").value);
+  let ope = document.querySelector("#operator").value;
+  let calculate = "";
+
+  if (ope == "add") {
+    calculate = a + b;
+  } else if (ope == "sub") {
+    calculate = a - b;
+  } else if (ope == "div") {
+    calculate = a / b;
+  } else if (ope == "mul") {
+    calculate = a * b;
+  }
+   switch (ope) {
+    case "add":
+      calculate = value1 + value2;
+      break;
+    case "sub":
+      calculate = value1 - value2;
+      break;
+    case "div":
+      calculate = value1 / value2;
+      break;
+    case "mul":
+      calculate = value1 * value2;
+    default:
+      calculate = "'not a number";
+      break;
+  } 
+  document.querySelector("#result").innerHTML = calculate;
+};
+*/
